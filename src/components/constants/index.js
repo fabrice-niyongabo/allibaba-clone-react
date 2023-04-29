3ab2f8f6116985f40a6cd023e0233b3d2e1aef76
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-04-29 17:59:20 +0300

    controllers

---
+++ src/components/constants/index.js
@@ -0,0 +1,16 @@
+export const appColors = {
+  GREEN: "#13d22d",
+  LIGHT_BLACK: "#17161e",
+  DARK_BLACK: "#121117",
+  GRAY: "#f3f3f3",
+  TEXT_WHITE: "#a1a1a3",
+  RED: "#fc3839",
+  WHITE: "#fff",
+  TEXT_GRAY: "#676767",
+};
+
+export const app = {
+  PUBLIC_URL: process.env.PUBLIC_URL,
+  BACKEND_URL: process.env.REACT_APP_BACKEND_URL,
+  FILE_URL: process.env.REACT_APP_BACKEND_FILE_URL,
+};
