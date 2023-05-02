60be9d6ba7e7e7862d2a8b602a360ab839d9d91d
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-05-02 18:05:23 +0300

    header fix

--- src/views/admin/home-categories/index.tsx
+++ src/views/admin/home-categories/index.tsx
@@ -151,6 +151,34 @@ const HomeCategories = () => {
             </CardBody>
           </Card>
         </Col>
+        <Col md={6}>
+          <Card>
+            <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+              Header Navbar section
+            </CardTitle>
+            <CardBody className="">
+              <div className="table-responsive">
+                <table className="table table-bordered">
+                  <tbody style={{ borderTopWidth: 0 }}>
+                    {categoriesReducer.categories.map((item, position) => (
+                      <tr key={position}>
+                        <td>{item.name}</td>
+                        <td>
+                          <Check
+                            callBack={handleChange}
+                            id={item.id}
+                            column="onHeaderNav"
+                            defaultValue={item.onHeaderNav}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
       </Row>
       <FullPageLoader open={isSubmitting} />
     </div>
