124fceabd3a292e7709057096842313d89a689bd
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-06-13 00:21:40 +0200

    search fix

--- src/components/header/mobile/index.tsx
+++ src/components/header/mobile/index.tsx
@@ -1,34 +1,86 @@
-import React from "react";
-import userImage from "../../../assets/images/user.png";
+import { useState, useEffect } from "react";
 import downArrow from "../../../assets/images/downarrow.png";
-import walletImage from "../../../assets/images/wallet.png";
-import wishListImage from "../../../assets/images/orders.png";
-import categoryBanner from "../../../assets/images/static/banner.jpg";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../reducers";
-import { IProduct, IUser, Ishop, USER_ROLE_ENUM } from "../../../interfaces";
-import { openUrlInNewTab } from "../../helpers";
+import { IProduct, Ishop } from "../../../interfaces";
 
 import logo from "../../../assets/images/logo2.png";
 
 function MobileHeader() {
-  const { token, role } = useSelector((state: RootState) => state.user);
   const navigate = useNavigate();
+  const { products } = useSelector((state: RootState) => state.products);
+  const { shops } = useSelector((state: RootState) => state.shops);
+
+  const [searchCategory, setSearchCategory] = useState("products");
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const [suggestions, setSuggestions] = useState<IProduct[]>([]);
+  const [shopSuggestions, setShopsSuggestions] = useState<Ishop[]>([]);
+
+  const [focused, setFocused] = useState<boolean>(false);
+  const [showSearch, setShowSearch] = useState<boolean>(false);
+
+  useEffect(() => {
+    let sub = true;
+    if (sub) {
+      if (searchKeyword.trim() !== "") {
+        if (searchCategory === "products") {
+          const sugge = products.filter((item) =>
+            item.name.toLowerCase().includes(searchKeyword.toLowerCase())
+          );
+          setSuggestions(sugge.slice(0, 5));
+        } else {
+          const sugge = shops.filter((item) =>
+            item.shopName.toLowerCase().includes(searchKeyword.toLowerCase())
+          );
+          setShopsSuggestions(sugge.slice(0, 5));
+        }
+      } else {
+        setSuggestions([]);
+        setShopsSuggestions([]);
+      }
+    }
+    return () => {
+      sub = false;
+    };
+  }, [products, searchKeyword, searchCategory]);
+
+  const handleSearch = () => {
+    if (searchKeyword.trim() === "") {
+      return;
+    }
+    navigate("/search/" + searchCategory + "/" + searchKeyword);
+  };
+
   return (
     <div className="mobile-main-container">
-      <div>
-        <i className="bi bi-list" style={{ fontSize: 20 }}></i>
-      </div>
       <div className="top-container">
-        <Link to="/">
-          <div className="logo-containter">
-            <img src={logo} alt="Afriseller" />
-            <span>Afriseller</span>
-          </div>
-        </Link>
-        {/* <div className="search-main-container">
+        <div>
+          <i className="bi bi-list" style={{ fontSize: 25 }}></i>
+        </div>
+        <div className="logo-container">
+          <Link to="/">
+            <div className="logo-containter">
+              <img src={logo} alt="Afriseller" />
+              <span>Afriseller</span>
+            </div>
+          </Link>
+        </div>
+        <div
+          className="icons-main-container"
+          onClick={() => setShowSearch(!showSearch)}
+        >
+          <i
+            className={`bi bi-${showSearch ? "x" : "search"}`}
+            style={{ fontSize: 25 }}
+          ></i>
+        </div>
+      </div>
+
+      {showSearch && (
+        <div className="search-main-container">
           <div className="search-container">
             <select
               className="col-md-3"
@@ -95,9 +147,8 @@ function MobileHeader() {
                 </ul>
               </div>
             )}
-        </div> */}
-        <div className="icons-main-container"></div>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
