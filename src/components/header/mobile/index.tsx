7bd846aa3e896add2f549a23fb0186361f60bb4d
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-06-13 00:31:39 +0200

    mobile menue

--- src/components/header/mobile/index.tsx
+++ src/components/header/mobile/index.tsx
@@ -55,101 +55,110 @@ function MobileHeader() {
   };
 
   return (
-    <div className="mobile-main-container">
-      <div className="top-container">
-        <div>
-          <i className="bi bi-list" style={{ fontSize: 25 }}></i>
+    <>
+      <div className="mobile-main-container">
+        <div className="top-container">
+          <div>
+            <i className="bi bi-list" style={{ fontSize: 25 }}></i>
+          </div>
+          <div className="logo-container">
+            <Link to="/">
+              <div className="logo-containter">
+                <img src={logo} alt="Afriseller" />
+                <span>Afriseller</span>
+              </div>
+            </Link>
+          </div>
+          <div
+            className="icons-main-container"
+            onClick={() => setShowSearch(!showSearch)}
+          >
+            <i
+              className={`bi bi-${showSearch ? "x" : "search"}`}
+              style={{ fontSize: 25 }}
+            ></i>
+          </div>
         </div>
-        <div className="logo-container">
-          <Link to="/">
-            <div className="logo-containter">
-              <img src={logo} alt="Afriseller" />
-              <span>Afriseller</span>
+
+        {showSearch && (
+          <div className="search-main-container">
+            <div className="search-container">
+              <select
+                className="col-md-3"
+                style={{
+                  background: `url(${downArrow}) right / 16px no-repeat #fff`,
+                }}
+                value={searchCategory}
+                onChange={(e) => setSearchCategory(e.target.value)}
+              >
+                <option value="products">Products</option>
+                <option value="seller">Seller</option>
+              </select>
+              <input
+                type="text"
+                value={searchKeyword}
+                onChange={(e) => setSearchKeyword(e.target.value)}
+                placeholder="What are you looking for..."
+                onFocus={() => setFocused(true)}
+                onBlur={() =>
+                  setTimeout(() => {
+                    setFocused(false);
+                  }, 500)
+                }
+              />
+              <button onClick={() => handleSearch()}>Search</button>
             </div>
-          </Link>
-        </div>
-        <div
-          className="icons-main-container"
-          onClick={() => setShowSearch(!showSearch)}
-        >
-          <i
-            className={`bi bi-${showSearch ? "x" : "search"}`}
-            style={{ fontSize: 25 }}
-          ></i>
-        </div>
+            {searchCategory === "products" &&
+              suggestions.length > 0 &&
+              focused && (
+                <div className="search-sugestions">
+                  <ul>
+                    {suggestions.map((item, index) => (
+                      <li
+                        onClick={() => {
+                          setSearchKeyword(item.name);
+                          navigate(
+                            "/search/" + searchCategory + "/" + item.name
+                          );
+                        }}
+                        key={index}
+                      >
+                        {item.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            {searchCategory === "seller" &&
+              shopSuggestions.length > 0 &&
+              focused && (
+                <div className="search-sugestions">
+                  <ul>
+                    {shopSuggestions.map((item, index) => (
+                      <li
+                        onClick={() => {
+                          setSearchKeyword(item.shopName);
+                          navigate(
+                            "/search/" + searchCategory + "/" + item.shopName
+                          );
+                        }}
+                        key={index}
+                      >
+                        {item.shopName}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+          </div>
+        )}
       </div>
 
-      {showSearch && (
-        <div className="search-main-container">
-          <div className="search-container">
-            <select
-              className="col-md-3"
-              style={{
-                background: `url(${downArrow}) right / 16px no-repeat #fff`,
-              }}
-              value={searchCategory}
-              onChange={(e) => setSearchCategory(e.target.value)}
-            >
-              <option value="products">Products</option>
-              <option value="seller">Seller</option>
-            </select>
-            <input
-              type="text"
-              value={searchKeyword}
-              onChange={(e) => setSearchKeyword(e.target.value)}
-              placeholder="What are you looking for..."
-              onFocus={() => setFocused(true)}
-              onBlur={() =>
-                setTimeout(() => {
-                  setFocused(false);
-                }, 500)
-              }
-            />
-            <button onClick={() => handleSearch()}>Search</button>
-          </div>
-          {searchCategory === "products" &&
-            suggestions.length > 0 &&
-            focused && (
-              <div className="search-sugestions">
-                <ul>
-                  {suggestions.map((item, index) => (
-                    <li
-                      onClick={() => {
-                        setSearchKeyword(item.name);
-                        navigate("/search/" + searchCategory + "/" + item.name);
-                      }}
-                      key={index}
-                    >
-                      {item.name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          {searchCategory === "seller" &&
-            shopSuggestions.length > 0 &&
-            focused && (
-              <div className="search-sugestions">
-                <ul>
-                  {shopSuggestions.map((item, index) => (
-                    <li
-                      onClick={() => {
-                        setSearchKeyword(item.shopName);
-                        navigate(
-                          "/search/" + searchCategory + "/" + item.shopName
-                        );
-                      }}
-                      key={index}
-                    >
-                      {item.shopName}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-        </div>
-      )}
-    </div>
+      {/* menus */}
+      <div className="mobile-menu">
+        <div className="menu-contents">hello</div>
+      </div>
+    </>
   );
 }
 
