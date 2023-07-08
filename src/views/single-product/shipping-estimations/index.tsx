0a45d21f32d81d98c34d367f2f5bb85c0c71b693
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-07-08 23:06:06 +0200

    fetching shipping estimations

--- src/views/single-product/shipping-estimations/index.tsx
+++ src/views/single-product/shipping-estimations/index.tsx
@@ -1,15 +1,28 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { IProduct } from "../../../interfaces";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../reducers";
+import { fetchShippingEstimations } from "../../../actions/shippingEstimations";
 
 interface Iprops {
-  product: IProduct;
+  product: IProduct | undefined;
 }
 function ShippingEstimations({ product }: Iprops) {
-  useEffect(() => {}, []);
+  const dispatch = useDispatch();
+  const { isLoading, estimaitons, supplierId } = useSelector(
+    (state: RootState) => state.estimations
+  );
+  const { shops } = useSelector((state: RootState) => state.shops);
+  useEffect(() => {
+    const shop = shops.find((item) => item.shopId === product?.shopId);
+    if (shop) {
+      dispatch(fetchShippingEstimations(shop.userId));
+    }
+  }, [product, shops]);
   return (
     <div style={{ marginTop: "1.5rem" }}>
       <h3>Shipping Estimations</h3>
