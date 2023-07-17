47a45eb888949efaa3c39616e46f7d54ad6868df
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-07-17 23:46:26 +0200

    removing from wishlist

--- src/views/wishlist/index.tsx
+++ src/views/wishlist/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Card, CardBody, CardTitle, Col, Row } from "reactstrap";
 import { RootState } from "../../reducers";
@@ -8,10 +8,41 @@ import ImageLoader from "../../components/image-loader";
 import { app } from "../../constants";
 import "../../assets/scss/wishlist.scss";
 import { Link } from "react-router-dom";
+import { IProduct, TOAST_MESSAGE_TYPES } from "../../interfaces";
+import Confirmation from "../../controllers/confirmation";
+import FullPageLoader from "../../components/full-page-loader";
+import axios from "axios";
+import { errorHandler, setHeaders, toastMessage } from "../../helpers";
 
 const WishList = () => {
   const dispatch = useDispatch();
+  const { token } = useSelector((state: RootState) => state.user);
   const { isLoading, list } = useSelector((state: RootState) => state.wishlist);
+  const [isSubmiting, setIsSubmiting] = useState<boolean>(false);
+  const [selectedItem, setSelectedItem] = useState<IProduct | undefined>(
+    undefined
+  );
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+
+  const handleDelete = () => {
+    setIsSubmiting(true);
+
+    axios
+      .delete(
+        app.BACKEND_URL + "/wishlist/" + selectedItem?.pId,
+        setHeaders(token)
+      )
+      .then((res) => {
+        setIsSubmiting(false);
+        toastMessage(TOAST_MESSAGE_TYPES.SUCCESS, res.data.msg);
+        dispatch(fetchWishlist());
+      })
+      .catch((error) => {
+        setIsSubmiting(false);
+        errorHandler(error);
+      });
+  };
+
   useEffect(() => {
     dispatch(fetchWishlist());
   }, []);
@@ -34,6 +65,15 @@ const WishList = () => {
                       <ImageLoader src={app.FILE_URL + item.images[0]?.image} />
                     </Link>
                     <p>{item.name}</p>
+                    <button
+                      className="common-btn"
+                      onClick={() => {
+                        setSelectedItem(item);
+                        setShowAlert(true);
+                      }}
+                    >
+                      <i className="bi bi-trash"></i> Remove
+                    </button>
                   </div>
                 </Col>
               ))}
@@ -41,6 +81,13 @@ const WishList = () => {
           )}
         </CardBody>
       </Card>
+      <Confirmation
+        title={`Do you want to remove ${selectedItem?.name} from your wishlist?`}
+        callback={handleDelete}
+        setShowAlert={setShowAlert}
+        showAlert={showAlert}
+      />
+      <FullPageLoader open={isSubmiting} />
     </div>
   );
 };
