fc5029caceb6cedd1dabba4992878ad8140116b9
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-07-19 12:38:42 +0200

    booking fix

--- src/views/single-product/book/index.tsx
+++ src/views/single-product/book/index.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
 import { Modal } from "react-bootstrap";
-import { IProduct } from "../../../interfaces";
+import { IProduct, TOAST_MESSAGE_TYPES } from "../../../interfaces";
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from "draft-js";
+import { EditorState, convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import { errorHandler, setHeaders, toastMessage } from "../../../helpers";
+import axios from "axios";
+import { app } from "../../../constants";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../reducers";
+import { fetchBooking } from "../../../actions/bookings";
+import FullPageLoader from "../../../components/full-page-loader";
 
 interface IProps {
   showModal: boolean;
@@ -18,12 +26,57 @@ const initialState = {
   confirm: false,
 };
 const Book = ({ showModal, setShowModal, product }: IProps) => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state: RootState) => state.user);
   const [state, setState] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
   const handleSubmit = (e: any) => {
     e.preventDefault();
+    const description = draftToHtml(
+      convertToRaw(state.description.getCurrentContent())
+    );
+    if (description.length < 10) {
+      toastMessage(
+        TOAST_MESSAGE_TYPES.INFO,
+        "Description/message can not be less that 10 characters please!"
+      );
+      return;
+    }
+    setIsLoading(true);
+    axios
+      .post(
+        app.BACKEND_URL + "/booking",
+        { ...state, description, productId: product?.pId },
+        setHeaders(token)
+      )
+      .then((res) => {
+        setIsLoading(false);
+        toastMessage(TOAST_MESSAGE_TYPES.SUCCESS, res.data.msg);
+        dispatch(fetchBooking());
+        setShowModal(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        errorHandler(error);
+      });
+  };
+
+  const handleDateChange = (type: "from" | "to", e: any) => {
+    if (e.target.value.trim() === "") {
+      setState({ ...state, [type]: e.target.value });
+      return;
+    }
+    const date = new Date();
+    const choosenDate = new Date(e.target.value);
+    if (choosenDate < date) {
+      toastMessage(TOAST_MESSAGE_TYPES.ERROR, "Please specify valid date");
+      return;
+    }
+    setState({ ...state, [type]: e.target.value });
   };
   return (
     <>
+      <FullPageLoader open={isLoading} />
       <Modal
         show={showModal}
         onHide={() => setShowModal(false)}
@@ -95,18 +148,30 @@ const Book = ({ showModal, setShowModal, product }: IProps) => {
                   <tr>
                     <td>
                       <b>From</b>
-                      <input type="date" className="form-control" />
+                      <input
+                        type="date"
+                        className="form-control"
+                        onChange={(e) => handleDateChange("from", e)}
+                        value={state.from}
+                        required
+                      />
                     </td>
                     <td>
                       <b>Up To</b>
-                      <input type="date" className="form-control" />
+                      <input
+                        type="date"
+                        className="form-control"
+                        onChange={(e) => handleDateChange("to", e)}
+                        value={state.to}
+                        required
+                      />
                     </td>
                   </tr>
                 </tbody>
               </table>
             </div>
             <div>
-              <input type="checkbox" />{" "}
+              <input type="checkbox" required />{" "}
               <span>
                 Share my contact details to the supplier so that I can be
                 contacted about processing of this order.
