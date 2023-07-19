d8791416a9293a11d88dec3c108f4f1a0c5e2fe0
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-07-19 13:02:05 +0200

    fx

--- src/views/logout/index.tsx
+++ src/views/logout/index.tsx
@@ -2,12 +2,16 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { resetUser } from "../../actions/user";
+import { resetBooking } from "../../actions/bookings";
+import { resetWishlist } from "../../actions/wishlist";
 
 const Logout = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(resetUser());
+    dispatch(resetBooking());
+    dispatch(resetWishlist());
     const url = new URL(window.location.href);
     const pathValue = url.searchParams.get("redirect");
     if (pathValue && pathValue.trim().length > 1) {
