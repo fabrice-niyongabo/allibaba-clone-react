af109ae8872696cde9a6fc7a718ce654cbed9942
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-07-19 17:22:08 +0200

    supplier view products

--- src/views/seller/bookings/view-product/index.tsx
+++ src/views/seller/bookings/view-product/index.tsx
@@ -1,6 +1,7 @@
 import { Modal } from "react-bootstrap";
 import ReactHtmlParser from "react-html-parser";
 import { IBookingForSupplier } from "../../../../interfaces";
+import { Col, Row } from "reactstrap";
 
 interface Iprops {
   showModal: boolean;
@@ -52,6 +53,24 @@ function ViewProduct({ setShowModal, showModal, booked }: Iprops) {
                 </tr>
               </tbody>
             </table>
+            <div className="border p-2">
+              <b>Client Details</b>
+              <hr />
+              <Row>
+                <Col md={6}>
+                  <b>Names</b>
+                  <p>{booked?.user.names}</p>
+                </Col>
+                <Col md={6}>
+                  <b>Email</b>
+                  <p>{booked?.user.email}</p>
+                </Col>
+                <Col md={6}>
+                  <b>Phone Number</b>
+                  <p>{booked?.user.phone}</p>
+                </Col>
+              </Row>
+            </div>
             <small>
               <i>{new Date(String(booked?.createdAt)).toDateString()}</i>
             </small>
