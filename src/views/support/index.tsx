627e589f16c05dc90db63e57c6356b09b984d132
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-16 00:08:23 +0200

    support page fix

--- src/views/support/index.tsx
+++ src/views/support/index.tsx
@@ -25,67 +25,72 @@ function Support() {
             <b>Support Channels</b>
             <p>
               We offer support through the following channels: Email Support:
-              Customers can reach out to our support team via email at
+              Customers can reach out to our support team via email at{" "}
               <a href="mailto:info@afrisellers.com">info@afrisellers.com</a>. We
               strive to respond to all emails within 2 hours during business
               hours.
             </p>
           </li>
           <li>
-            <b></b>
-            <p></p>
+            <b>Types of Support</b>
+            <p>
+              We provide support for the following: Technical Issues: Assistance
+              with any technical issues related to the Afrisellers.com website's
+              functionality. Order Inquiries: Information and assistance
+              regarding order status, tracking, and shipping. Product
+              Information: Help with product details, specifications, and
+              recommendations. Payment and Billing: Support for payment-related
+              issues and billing inquiries.
+            </p>
           </li>
           <li>
-            <b></b>
-            <p></p>
+            <b>Exclusions</b>
+            <p>
+              Our support policy does not cover the following: Issues arising
+              from the use of outdated browsers or incompatible devices.
+              Third-party products or services not directly related to
+              Afrisellers.com. Customization requests beyond the scope of our
+              standard offerings.
+            </p>
           </li>
           <li>
-            <b></b>
-            <p></p>
+            <b>Maintenance and Downtime</b>
+            <p>
+              We may schedule maintenance activities to improve the website's
+              performance. Prior notice will be provided for any planned
+              downtime, and we aim to minimize disruptions to the best of our
+              ability.
+            </p>
           </li>
           <li>
-            <b></b>
-            <p></p>
+            <b>Feedback</b>
+            <p>
+              We value customer feedback and encourage users to provide
+              suggestions or report any issues they encounter. This helps us
+              continually improve our services and enhance the overall customer
+              experience.
+            </p>
           </li>
           <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
-          </li>
-          <li>
-            <b></b>
-            <p></p>
+            <b>Policy Updates</b>
+            <p>
+              This Support Policy may be updated from time to time. Any changes
+              will be communicated to customers through our website and other
+              appropriate channels.
+            </p>
+            <p>
+              By using Afrisellers.com and seeking support, customers agree to
+              abide by the terms outlined in this Support Policy.
+            </p>
+            <p>
+              If you have any questions or concerns regarding our support
+              services, please contact our support team at{" "}
+              <a href="mailto:info@afrisellers.com">info@afrisellers.com</a>.
+            </p>
+            <p>
+              Thank you for choosing Afrisellers.com for your e-commerce needs.
+              We appreciate your business.
+            </p>
           </li>
         </ol>
         <p>
